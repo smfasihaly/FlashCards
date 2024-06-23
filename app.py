580caf271b2cfecacce5918d11e9c08fe7b87104
smfasihaly <smfasihali@gmail.com>
2024-06-23 03:48:45 +0200

    new feature multiusers

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, session
 import pandas as pd
 import random
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
 # Read the Excel file
-excel_file_path = 'Data/words.xlsx'
+excel_file_path = 'learnITalian/Data/words.xlsx'
 data = pd.read_excel(excel_file_path)
 
 # Convert the data to a list of dictionaries
@@ -13,11 +15,15 @@ verbs_data = data.to_dict(orient='records')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    user_logged_in = 'user' in session
+    random.shuffle(verbs_data)
+    return render_template('index.html', user_logged_in=user_logged_in)
 
 @app.route('/get_verbs')
 def get_verbs():
-    random.shuffle(verbs_data)
+    if 'user' not in session:
+        return jsonify({"error": "User not logged in", "data": []}), 401  # 401 Unauthorized
+
     page = int(request.args.get('page', 1))
     per_page = int(request.args.get('per_page', 9))
     start = (page - 1) * per_page
@@ -32,9 +38,15 @@ def get_verbs():
 
 @app.route('/get_verbs/<sheet_name>')
 def get_verbs_from_sheet(sheet_name):
+    username = session.get('user')  # Assuming the username is stored in session
+    if not username:
+        return jsonify({"error": "User not logged in"}), 401
+
     try:
         data = pd.read_excel(excel_file_path, sheet_name=sheet_name)
-        sheet_data = data.to_dict(orient='records')
+        # Filter data by username
+        filtered_data = data[data['Username'] == username]
+        sheet_data = filtered_data.to_dict(orient='records')
         random.shuffle(sheet_data)
         page = int(request.args.get('page', 1))
         per_page = int(request.args.get('per_page', 10))
@@ -49,20 +61,65 @@ def get_verbs_from_sheet(sheet_name):
         return jsonify(response_data)
     except Exception as e:
         return jsonify({"error": str(e)}), 400
-
 @app.route('/save_stats', methods=['POST'])
 def save_stats():
     stats_data = request.json
     just_flipped = stats_data.get('justFlipped', [])
     failure = stats_data.get('failure', [])
-    
-    with pd.ExcelWriter(excel_file_path, mode='a', if_sheet_exists='replace') as writer:
+    username = session['user']
+
+    with pd.ExcelWriter(excel_file_path, engine="openpyxl", mode='a', if_sheet_exists='replace') as writer:
         if just_flipped:
-            pd.DataFrame(just_flipped, columns=['Italian', 'English']).to_excel(writer, sheet_name='JustFlipped', index=False)
+            just_flipped_df = pd.DataFrame(just_flipped, columns=['Italian', 'English'])
+            just_flipped_df['Username'] = username  # Add the username column
+            just_flipped_df.to_excel(writer, sheet_name='JustFlipped', index=False)
         if failure:
-            pd.DataFrame(failure, columns=['Italian', 'English']).to_excel(writer, sheet_name='Failure', index=False)
-    
+            failure_df = pd.DataFrame(failure, columns=['Italian', 'English'])
+            failure_df['Username'] = username  # Add the username column
+            failure_df.to_excel(writer, sheet_name='Failure', index=False)
+
     return jsonify({"status": "success"})
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+@app.route('/signup', methods=['POST'])
+def signup():
+    user_data = request.json
+    username = user_data.get('username')
+    password = user_data.get('password')
+
+    try:
+        existing_data = pd.read_excel(excel_file_path, sheet_name='Users')
+        if username in existing_data['Username'].values:
+            return jsonify({"error": "Username already exists"}), 400
+        new_user_data = existing_data.append({'Username': username, 'Password': generate_password_hash(password)}, ignore_index=True)
+    except Exception as e:
+        new_user_data = pd.DataFrame([{'Username': username, 'Password': generate_password_hash(password)}])
+
+    with pd.ExcelWriter(excel_file_path, engine="openpyxl", mode='a', if_sheet_exists='replace') as writer:
+        new_user_data.to_excel(writer, sheet_name='Users', index=False)
+
+    return jsonify({"status": "success"})
+
+@app.route('/login', methods=['POST'])
+def login():
+    user_data = request.json
+    username = user_data.get('username')
+    password = user_data.get('password')
+
+    try:
+        existing_data = pd.read_excel(excel_file_path, sheet_name='Users')
+        user_row = existing_data[existing_data['Username'] == username].iloc[0]
+        if check_password_hash(user_row['Password'], password):
+            session['user'] = username  # Set session for the logged-in user
+            return jsonify({"status": "success"})
+        else:
+            return jsonify({"error": "Invalid username or password"}), 400
+    except Exception as e:
+        return jsonify({"error": "Invalid username or password"}), 400
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    session.clear()  # Clear the session
+    return jsonify({"status": "success"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,45 +1,170 @@
 document.addEventListener('DOMContentLoaded', () => {
     let currentPage = 1;
     const perPage = 9;
+    const loginButton = document.getElementById('login-button');
+    const logoutButton = document.getElementById('logout-button');
+    const loginForm = document.getElementById('login-form');
+    const signupForm = document.getElementById('signup-form');
+    const showSignupFormLink = document.getElementById('show-signup-form');
+    const showLoginFormLink = document.getElementById('show-login-form');
+    const modalTitle = document.getElementById('modal-title');
+    const loginModal = document.getElementById('login-modal');
 
-    const fetchData = (page = 1) => {
+    loginModal.style.display = userLoggedIn ? 'none' : 'block';
+    loginButton.style.display = userLoggedIn ? 'none' : 'block';
+    logoutButton.style.display = userLoggedIn ? 'block' : 'none';
+    if (userLoggedIn) fetchData(1);
+
+    // Fetch data function
+    function fetchData(page) {
         fetch(`/get_verbs?page=${page}&per_page=${perPage}`)
+            .then(response => {
+                if (!response.ok) throw new Error('Not authorized');
+                return response.json();
+            })
+            .then(data => {
+                if (data.verbs) {
+                    displayCards(data.verbs);
+                    updatePaginationControls(data.total_pages, data.current_page);
+                    restoreCardStates(page);
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching verbs:', error);
+                alert('Please log in to view data.');
+            });
+    }
+
+    sessionStorage.clear();
+
+    loginButton.addEventListener('click', () => loginModal.style.display = 'block');
+
+    logoutButton.addEventListener('click', () => {
+        fetch('/logout', { method: 'POST' })
             .then(response => response.json())
             .then(data => {
-                displayCards(data.verbs);
-                updatePaginationControls(data.total_pages, data.current_page);
+                if (data.status === 'success') {
+                    logoutButton.style.display = 'none';
+                    loginButton.style.display = 'block';
+                    loginModal.style.display = 'block';
+                    alert('Logged out successfully!');
+                }
             })
-            .catch(error => console.error('Error fetching verbs:', error));
-    };
+            .catch(error => console.error('Error during logout:', error));
+    });
 
-    fetchData(currentPage);
+    showSignupFormLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        loginForm.style.display = 'none';
+        signupForm.style.display = 'block';
+        modalTitle.textContent = 'Sign Up';
+    });
+
+    showLoginFormLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        signupForm.style.display = 'none';
+        loginForm.style.display = 'block';
+        modalTitle.textContent = 'Login';
+    });
+
+    loginForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const username = document.getElementById('login-username').value;
+        const password = document.getElementById('login-password').value;
+
+        fetch('/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.status === 'success') {
+                    alert('Login successful!');
+                    loginModal.style.display = 'none';
+                    loginButton.style.display = 'none';
+                    logoutButton.style.display = 'block';
+                    fetchData(1);
+                } else {
+                    alert('Error: ' + data.error);
+                }
+            })
+            .catch(error => console.error('Error during login:', error));
+    });
+
+    signupForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const username = document.getElementById('signup-username').value;
+        const password = document.getElementById('signup-password').value;
+
+        fetch('/signup', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password })
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.status === 'success') {
+                    alert('Signup successful! Please log in.');
+                    signupForm.style.display = 'none';
+                    loginForm.style.display = 'block';
+                    modalTitle.textContent = 'Login';
+                } else {
+                    alert('Error: ' + data.error);
+                }
+            })
+            .catch(error => console.error('Error during signup:', error));
+    });
 
     document.getElementById('stats-button').addEventListener('click', showStats);
-    document.getElementById('show-all-button').addEventListener('click', () => loadVerbs('Sheet1', currentPage));
+    document.getElementById('show-all-button').addEventListener('click', () => fetchData(currentPage));
     document.getElementById('previous-failed-button').addEventListener('click', () => loadVerbs('Failure', currentPage));
     document.getElementById('previous-flipped-button').addEventListener('click', () => loadVerbs('JustFlipped', currentPage));
 
     document.getElementById('prev-page').addEventListener('click', () => {
         if (currentPage > 1) {
+            saveCardStates(currentPage);
             currentPage--;
             fetchData(currentPage);
         }
     });
 
     document.getElementById('next-page').addEventListener('click', () => {
+        saveCardStates(currentPage);
         currentPage++;
         fetchData(currentPage);
     });
 
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'Escape') {
-            closeStats();
+    function saveCardStates(page) {
+        const cardStates = [];
+        document.querySelectorAll('.flip-card-inner').forEach((card, index) => {
+            const isFlipped = card.classList.contains('flipped');
+            const cardColor = document.getElementById(`flip-card-back-${index}`).style.backgroundColor;
+            cardStates[index] = { isFlipped, cardColor };
+        });
+        sessionStorage.setItem(`cardStates-${page}`, JSON.stringify(cardStates));
+    }
+
+    function restoreCardStates(page) {
+        const savedStates = JSON.parse(sessionStorage.getItem(`cardStates-${page}`));
+        if (savedStates) {
+            document.querySelectorAll('.flip-card-inner').forEach((card, index) => {
+                if (savedStates[index].isFlipped) {
+                    card.classList.add('flipped');
+                    document.getElementById(`flip-card-back-${index}`).style.backgroundColor = savedStates[index].cardColor;
+                }
+            });
         }
+    }
+
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') closeStats();
     });
 
     window.addEventListener('beforeunload', sendStatsToServer);
 
     function loadVerbs(sheetName, page = 1) {
+        sessionStorage.clear();
         fetch(`/get_verbs/${sheetName}?page=${page}&per_page=${perPage}`)
             .then(response => response.json())
             .then(data => {
@@ -51,19 +176,17 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             })
             .catch(error => console.error('Error loading verbs:', error));
+
+        stats = { success: 0, failure: 0, justFlipped: 0, justFlippedCards: [], failureCards: [] };
+        cardStates = [];
     }
+
+
 
     function displayCards(data) {
         const container = document.getElementById('cards-container');
         container.innerHTML = '';  // Clear previous cards
-        stats = {
-            success: 0,
-            failure: 0,
-            justFlipped: 0,
-            justFlippedCards: [],
-            failureCards: []
-        };
-        cardStates = [];
+
         data.forEach((verb, index) => {
             const card = document.createElement('div');
             card.className = 'flip-card';
@@ -71,7 +194,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 <div class="flip-card-inner" id="card-inner-${index}">
                     <div class="flip-card-front">
                         <h2>${verb['Italian']}</h2>
-                        <input type="text"  autoComplete="off" placeholder="enter here" id="inputValue-${index}" onkeydown="checkEnter(event, ${index})" onclick="event.stopPropagation()" />
+                        <input type="text" autocomplete="off" placeholder="enter here" id="inputValue-${index}" onkeydown="checkEnter(event, ${index}, ${currentPage})" onclick="event.stopPropagation()" />
                     </div>
                     <div class="flip-card-back" id="flip-card-back-${index}">
                         <h2>${verb['English']}</h2>
@@ -80,7 +203,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 </div>
             `;
             card.addEventListener('click', (event) => {
-                flipCard(card, index);
+                flipCard(card, index, currentPage);
             });
             container.appendChild(card);
         });
@@ -89,34 +212,52 @@ document.addEventListener('DOMContentLoaded', () => {
     function updatePaginationControls(totalPages, currentPage) {
         const pageInfo = document.getElementById('page-info');
         pageInfo.textContent = `Page ${currentPage} of ${totalPages}`;
-        
+
         const prevButton = document.getElementById('prev-page');
         const nextButton = document.getElementById('next-page');
 
-        if (currentPage === 1) {
-            prevButton.disabled = true;
-        } else {
-            prevButton.disabled = false;
-        }
-
-        if (currentPage === totalPages) {
-            nextButton.disabled = true;
-        } else {
-            nextButton.disabled = false;
-        }
+        prevButton.disabled = currentPage === 1;
+        nextButton.disabled = currentPage === totalPages;
     }
-    document.getElementById('stats-button').addEventListener('click', showStats);
-    document.getElementById('show-all-button').addEventListener('click', () => loadVerbs('Sheet1'));
-    document.getElementById('previous-failed-button').addEventListener('click', () => loadVerbs('Failure'));
-    document.getElementById('previous-flipped-button').addEventListener('click', () => loadVerbs('JustFlipped'));
 
-    document.addEventListener('keydown', (event) => {
-        if (event.key === 'Escape') {
-            closeStats();
-        }
-    });
 
-    window.addEventListener('beforeunload', sendStatsToServer);
+
+    function sendStatsToServer() {
+        const statsData = {
+            justFlipped: stats.justFlippedCards,
+            failure: stats.failureCards,
+           
+        };
+    
+        fetch('/save_stats', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(statsData)
+        })
+            .then(response => response.json())
+            .then(data => console.log('Statistics saved:', data))
+            .catch(error => console.error('Error saving statistics:', error));
+    }
+    
+
+    function showStats() {
+        const statsOutput = document.getElementById('stats-output');
+        statsOutput.innerHTML = `
+            <button id="close-stats">&times;</button>
+            <p>Success: ${stats.success}</p>
+            <p>Failure: ${stats.failure}</p>
+            <p>Just Flipped: ${stats.justFlipped}</p>
+        `;
+        statsOutput.style.display = 'block';
+        document.querySelector('.cards-container').classList.add('blur');
+        document.getElementById('close-stats').addEventListener('click', closeStats);
+        sendStatsToServer();
+    }
+
+    function closeStats() {
+        document.getElementById('stats-output').style.display = 'none';
+        document.querySelector('.cards-container').classList.remove('blur');
+    }
 });
 
 let stats = {
@@ -127,62 +268,25 @@ let stats = {
     failureCards: []
 };
 let cardStates = [];
-
-function displayCards(data) {
-    const container = document.getElementById('cards-container');
-    container.innerHTML = '';  // Clear previous cards
-    data.forEach((verb, index) => {
-        const card = document.createElement('div');
-        card.className = 'flip-card';
-        card.innerHTML = `
-            <div class="flip-card-inner" id="card-inner-${index}">
-                <div class="flip-card-front">
-                    <h2>${verb['Italian']}</h2>
-                    <input type="text" placeholder="enter here" id="inputValue-${index}" onkeydown="checkEnter(event, ${index})" onclick="event.stopPropagation()" />
-                </div>
-                <div class="flip-card-back" id="flip-card-back-${index}">
-                    <h2>${verb['English']}</h2>
-                    <p id="outputMessage-${index}"></p>
-                </div>
-            </div>
-        `;
-        card.addEventListener('click', (event) => {
-            flipCard(card, index);
-        });
-        container.appendChild(card);
-    });
-}
-
-function loadVerbs(sheetName) {
-    fetch(`/get_verbs/${sheetName}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.error) {
-                console.error(data.error);
-            } else {
-                displayCards(data);
-            }
-        })
-        .catch(error => console.error('Error loading verbs:', error));
-}
-
-function checkEnter(event, index) {
+function checkEnter(event, index, currentPage) {
     if (event.key === 'Enter') {
         const card = event.target.closest('.flip-card');
-        flipCard(card, index);
+        flipCard(card, index, currentPage);
     }
 }
-
-function flipCard(card, index) {
+function flipCard(card, index, page) {
     const innerCard = document.getElementById(`card-inner-${index}`);
     const inputValue = document.getElementById(`inputValue-${index}`).value.toLowerCase();
     const backTitle = document.querySelector(`#card-inner-${index} .flip-card-back h2`).textContent.toLowerCase();
     const outputMessage = document.getElementById(`outputMessage-${index}`);
     const flipCardBack = document.getElementById(`flip-card-back-${index}`);
+    const flipped = innerCard.classList.contains('flipped');
 
-    if (!innerCard.classList.contains('flipped') && !cardStates[index]) {
+    const cardStateKey = `cardState-${page}-${index}`;
+
+    if (!innerCard.classList.contains('flipped') && !cardStates[cardStateKey]) {
         document.getElementById(`inputValue-${index}`).disabled = true;
-        cardStates[index] = true;
+        cardStates[cardStateKey] = true;
         const inputValues = inputValue.split(',').map(value => value.trim().replace(/to /g, ''));
         const significantTitles = backTitle.split(',').map(title => title.trim().replace(/to /g, ''));
 
@@ -210,46 +314,7 @@ function flipCard(card, index) {
             });
         }
     }
-    
+
     innerCard.classList.toggle('flipped');
-}
-
-function sendStatsToServer() {
-    const statsData = {
-        justFlipped: stats.justFlippedCards,
-        failure: stats.failureCards
-    };
-
-    fetch('/save_stats', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(statsData)
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Statistics saved:', data);
-        
-    })
-    .catch(error => console.error('Error saving statistics:', error));
-}
-
-function showStats() {
-    const statsOutput = document.getElementById('stats-output');
-    statsOutput.innerHTML = `
-        <button id="close-stats">&times;</button>
-        <p>Success: ${stats.success}</p>
-        <p>Failure: ${stats.failure}</p>
-        <p>Just Flipped: ${stats.justFlipped}</p>
-    `;
-    statsOutput.style.display = 'block';
-    document.querySelector('.cards-container').classList.add('blur');
-    document.getElementById('close-stats').addEventListener('click', closeStats);
-}
-
-function closeStats() {
-    document.getElementById('stats-output').style.display = 'none';
-    document.querySelector('.cards-container').classList.remove('blur');
-    sendStatsToServer();
+    sessionStorage.setItem(cardStateKey, JSON.stringify({ flipped: !flipped }));
 }
